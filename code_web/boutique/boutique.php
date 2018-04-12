4e6b29f08e638b675eedf4f5a906b74084ae6064
WilliamNunesse <william.nunesse@viacesi.fr>
2018-04-12 11:40:39 +0200

    Edit menu fixed

---
+++ code_web/boutique/boutique.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Boutique BDE Cesi Lyon</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<meta charset="UTF-8">
+</head>
+<body>
+	<nav>
+		<a href="index.html" id="home_button"><img src="../../images/home_button.png" id="home_button_img"></a>
+		<ul>
+			<li class="nav_element"><a href="../boite_a_idees/boite_a_idees.php">Boîte à idées</a>
+				<ul class="submenu">
+					<li><a href="../boite_a_idees/proposer_idee.php">Proposer une idée</a></li>
+					<li><a href="../boite_a_idees/voter_idee.php">Voter pour une idée</a></li>
+				</ul>
+			</li>
+		</ul>
+		<ul>
+			<li class="nav_element"><a href="../evenements/evenement_a_venir.php">Evenements à venir</a>
+			</li>
+		</ul>
+		<ul>
+			<li class="nav_element"><a href="../evenements/evenement_du_mois.php">Evenements du mois</a>
+			</li>
+		</ul>
+		<ul>
+			<li class="nav_element"><a href="../boutique/boutique.php">Boutique</a>
+			</li>
+		</ul>
+		<ul>
+			<li class="nav_element"><a href="../contact/contact.php">Contactez nous</a>
+			</li>
+		</ul>
+	</nav>
+	<section id="left_menu">
+			<a href="https://www.facebook.com/BDECesiLyon"><img src="../../images/facebook_logo.png"></a>
+			<a href="https://twitter.com/cesi_sudest"><img src="../../images/twitter_logo.png"></a>
+			<a href="https://www.youtube.com/watch?v=IyIDO3sI4Hw"><img src="../../images/youtube_logo.png"></a>
+	</section>
+
+</body>
+</html>
