cbfa3153a7fa1f282b8a708a084d0674883f6e4e
WilliamNunesse <william.nunesse@viacesi.fr>
2018-04-17 15:25:28 +0200

    Test

--- code_web/boite_a_idees/proposer_idee.php
+++ code_web/boite_a_idees/proposer_idee.php
@@ -52,6 +52,9 @@
 			<input type="text" name="titre_idee">
 			<p>La description de votre idée: </p>
 			<textarea name="description" rows="8" cols="60"></textarea>
+			<p>La date à laquelle vous aimeriez faire votre événement (vous n'êtes pas obligé de rentrer une valeur)</p>
+			<input type="date" name="date_event">
+			<p>C'est bon ?</p>
 			<input type="submit" name="valider">
 
 		</form>
