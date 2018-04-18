7a469dad6027ed049aa0d5b6d5a205fced15e49b
WilliamNunesse <william.nunesse@viacesi.fr>
2018-04-18 13:46:13 +0200

    Des petites modifs

--- code_web/inscription/sign_up.php
+++ code_web/inscription/sign_up.php
@@ -8,40 +8,11 @@ include("Connect.php");
 <html>
 	<head>
 		<meta charset="utf-8" name="viewport" content="width=device-width, initial-scale=1"/>
-		<link rel="stylesheet" href="../evenements/style.css" type="text/css" />
         <link rel="stylesheet" href="../inscription/style.css" type="text/css"/>
+        <link rel="stylesheet" href="../menu.css" type="text/css"/>
 		<title>Inscription BDE Cesi Lyon</title>
 	</head>
 	<body>
-     <div>
-		<nav>
-			<a href="index.html" id="home_button"><img src="../../images/home_button.png" id="home_button_img"></a>
-			<ul>
-				<li class="nav_element"><a href="../boite_a_idees/boite_a_idees.php">Boîte à idées</a>
-					<ul class="submenu">
-						<li><a href="../boite_a_idees/proposer_idee.php">Proposer une idée</a></li>
-						<li><a href="../boite_a_idees/voter_idee.php">Voter pour une idée</a></li>
-					</ul>
-				</li>
-			</ul>
-			<ul>
-				<li class="nav_element"><a href="../evenements/evenement_a_venir.php">Evenements à venir</a>
-				</li>
-			</ul>
-			<ul>
-				<li class="nav_element"><a href="../evenements/evenement_du_mois.php">Evenements du mois</a>
-				</li>
-			</ul>
-			<ul>
-				<li class="nav_element"><a href="../boutique/boutique.php">Boutique</a>
-				</li>
-			</ul>
-			<ul>
-				<li class="nav_element"><a href="../contact/contact.php">Contactez nous</a>
-				</li>
-			</ul>
-		</nav>
-    </div>
         
 		<Section id ="content">
 			<div>
