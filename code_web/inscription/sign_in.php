50e6528e3db1eb7c85c3188ece44992fa5bb719a
eliatib <thibault.michel@viacesi.fr>
2018-04-18 14:15:28 +0200

    inscription with redirection

--- code_web/inscription/sign_in.php
+++ code_web/inscription/sign_in.php
@@ -8,6 +8,7 @@ if(!isset($_POST['mail'],$_POST['mdp'])){
 echo '<html>
         <head>
             <meta charset="utf-8" name="viewport" content="width=device-width, initial-scale=1"/>
+            <link rel="stylesheet" href="../menu.css" type="text/css" />
             <link rel="stylesheet" href="../inscription/style.css" type="text/css"/>
             <title>Connexion BDE Cesi Lyon</title>
         </head>
@@ -113,42 +114,6 @@ else
                 <title>Connexion BDE Cesi Lyon</title>
             </head>
             <body>
-             <div>
-                <nav>
-                    <a href="index.html" id="home_button"><img src="../../images/home_button.png" id="home_button_img"></a>
-                    <ul>
-                        <li class="nav_element"><a href="../boite_a_idees/boite_a_idees.php">Boîte à idées</a>
-                            <ul class="submenu">
-                                <li><a href="../boite_a_idees/proposer_idee.php">Proposer une idée</a></li>
-                                <li><a href="../boite_a_idees/voter_idee.php">Voter pour une idée</a></li>
-                            </ul>
-                        </li>
-                    </ul>
-                    <ul>
-                        <li class="nav_element"><a href="../evenements/evenement_a_venir.php">Evenements à venir</a>
-                        </li>
-                    </ul>
-                    <ul>
-                        <li class="nav_element"><a href="../evenements/evenement_du_mois.php">Evenements du mois</a>
-                        </li>
-                    </ul>
-                    <ul>
-                        <li class="nav_element"><a href="../boutique/boutique.php">Boutique</a>
-                        </li>
-                    </ul>
-                    <ul>
-                        <li class="nav_element"><a href="../contact/contact.php">Contactez nous</a>
-                        </li>
-                    </ul>
-                    <ul>
-                        <li class="nav_element">
-                        <a href="../inscription/sign_out.php">
-                            Déconnexion
-                        </a>
-                        </li>
-                    </ul>
-                </nav>
-            </div>
 
                 <Section id ="content">
                     <div>
@@ -161,6 +126,7 @@ else
                             </div>	
                         </div>
                     </div>
+                    <script>setTimeout("location.href = \'Accueil.php\';",2000);</script>;
                 </Section>
             </body>
         </html>'; }
--- code_web/inscription/sign_out.php
+++ code_web/inscription/sign_out.php
@@ -6,46 +6,11 @@ $titre="Déconnexion";
     echo '<html>
             <head>
                 <meta charset="utf-8" name="viewport" content="width=device-width, initial-scale=1"/>
+                <link rel="stylesheet" href="../menu.css" type="text/css" />
                 <link rel="stylesheet" href="../inscription/style.css" type="text/css"/>
                 <title>Connexion BDE Cesi Lyon</title>
             </head>
             <body>
-             <div>
-                <nav>
-                    <a href="index.html" id="home_button"><img src="../../images/home_button.png" id="home_button_img"></a>
-                    <ul>
-                        <li class="nav_element"><a href="../boite_a_idees/boite_a_idees.php">Boîte à idées</a>
-                            <ul class="submenu">
-                                <li><a href="../boite_a_idees/proposer_idee.php">Proposer une idée</a></li>
-                                <li><a href="../boite_a_idees/voter_idee.php">Voter pour une idée</a></li>
-                            </ul>
-                        </li>
-                    </ul>
-                    <ul>
-                        <li class="nav_element"><a href="../evenements/evenement_a_venir.php">Evenements à venir</a>
-                        </li>
-                    </ul>
-                    <ul>
-                        <li class="nav_element"><a href="../evenements/evenement_du_mois.php">Evenements du mois</a>
-                        </li>
-                    </ul>
-                    <ul>
-                        <li class="nav_element"><a href="../boutique/boutique.php">Boutique</a>
-                        </li>
-                    </ul>
-                    <ul>
-                        <li class="nav_element"><a href="../contact/contact.php">Contactez nous</a>
-                        </li>
-                    </ul>
-                    <ul>
-                        <li class="nav_element">
-                        <a href="../inscription/sign_out.php">
-                            Déconnexion
-                        </a>
-                        </li>
-                    </ul>
-                </nav>
-            </div>
             
                <Section id ="content">
                     <div>
@@ -54,6 +19,7 @@ $titre="Déconnexion";
                                 <div class="contact-w3-agileits">
                                 <h3>Deconnexion effectuée</h3>
                                 <a href="sign_in.php"><h3>Revenir à la page de connexion</h3></a>
+                                <script>setTimeout("location.href = \'sign_in.php\';",2000);</script>;
                             </div>	
                         </div>
                     </div>
