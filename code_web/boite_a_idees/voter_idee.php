247f60c67c966a566d56cf1f7bfa146455c09c4c
eliatib <thibault.michel@viacesi.fr>
2018-04-19 15:05:38 +0200

    nav

--- code_web/boite_a_idees/voter_idee.php
+++ code_web/boite_a_idees/voter_idee.php
@@ -36,37 +36,8 @@ session_start();
     </script>
 </head>
 <body>
-	<nav> <!-- Ici nous avons dans la balise nav le menu de navigation du haut, avec tous les liens des pages de notre site. Le CSS de tous les éléments menu est dans menu.css -->
-		<a href="index.html" id="home_button"><img src="../../images/home_button.png" id="home_button_img"></a>
-		<ul>
-			<li class="nav_element"><a href="../evenements/evenement_a_venir.php">Evenements à venir</a>
-			</li>
-		</ul>
-		<ul>
-			<li class="nav_element"><a href="../evenements/evenement_du_mois.php">Evenements du mois</a>
-			</li>
-		</ul>
-		<ul>
-			<li class="nav_element"><a href="../boite_a_idees/boite_a_idees.php">Boîte à idées</a>
-				<ul class="submenu"> <!-- Ici nous avons le sous-menu de boîte à idée -->
-					<li><a href="../boite_a_idees/proposer_idee.php">Proposer une idée</a></li>
-					<li><a href="../boite_a_idees/voter_idee.php">Voter pour une idée</a></li>
-				</ul>
-			</li>
-		</ul>
-		<ul>
-			<li class="nav_element"><a href="../boutique/boutique.php">Boutique</a>
-			</li>
-		</ul>
-		<ul>
-			<li class="nav_element"><a href="../contact/contact.php">Contactez nous</a>
-			</li>
-		</ul>
-        <ul>
-			<li class="nav_element"><a href="../inscription/sign_out.php">Deconnexion</a>
-			</li>
-		</ul>
-	</n<?php
+
+<?php
         if($_SESSION['id']==0)
         {
             echo '		
